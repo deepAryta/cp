1a0d1453a9d2fec126234e51d30a9b2c5895f48c
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2020-07-10 22:15:22 +0530

    new add ups

---
+++ coursera/week1/Main.java
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+// 		System.out.println("Hello World");
+	
+	Scanner in = new Scanner(System.in);
+	int n = in.nextInt();
+	int[] a = new int[200000];
+	int l1 = 0,indexofl1=-1;
+	int i,l2 = 0;
+	for(i=0;i<n;i++){
+	    a[i]=in.nextInt();
+	    if(l1<a[i]){
+	        l1=a[i];
+	        indexofl1=i;
+	    }
+	}
+	for(i=0;i<n;i++){
+	    if(indexofl1!=i&&l2<a[i]){
+	        l2=a[i];
+	    }
+	}
+	long ll1 = (long) l1;
+	long ll2 = (long) l2;
+	long  res = ll1*ll2;
+	System.out.println(res);
+	
+	    
+	    
+	}
+}
